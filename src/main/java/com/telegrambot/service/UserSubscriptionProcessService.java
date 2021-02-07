276de1da2cf82d8bb0250df1ae0c8c2c251baccd
EnyaevD <enyaev.danzan@gmail.com>
2021-02-07 17:19:48 +0300

    rewrited scheduler

--- src/main/java/com/telegrambot/service/UserSubscriptionProcessService.java
+++ src/main/java/com/telegrambot/service/UserSubscriptionProcessService.java
@@ -1,9 +1,9 @@
 package com.telegrambot.service;
 
 import com.telegrambot.bot.TelegramBot;
+import com.telegrambot.model.Item;
 import com.telegrambot.model.UserSubscription;
 import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -13,28 +13,27 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class UserSubscriptionProcessService {
 
-    private UserSubscriptionService subscriptionService;
-    private ReplyMessagesService messagesService;
-    private TelegramBot telegramBot;
+    private final UserSubscriptionService subscriptionService;
+    private final ReplyMessagesService messagesService;
+    private final TelegramBot telegramBot;
+    private final WebParserService webParserService;
 
-//    @Scheduled(fixedRateString = "${subscriptions.processPeriod}")
+    @Scheduled(fixedRateString = "${subscriptions.processPeriod}")
     public void processAllUsersSubscriptions() {
         log.info("Выполняю обработку подписок пользователей.");
-        subscriptionService.getAllSubscriptions().forEach(this::processSubscription);
+        if (!subscriptionService.getAllSubscriptions().isEmpty()) {
+            subscriptionService.getAllSubscriptions().forEach(this::processSubscription);
+        }
         log.info("Завершил обработку подписок пользователей.");
     }
-    //TODO
+
     private void processSubscription(UserSubscription subscription) {
-        subscription.getChatId();
-    }
-    // TODO
-    private void sendUserNotification(UserSubscription subscription, String priceChangeMessage) {
-        StringBuilder notificationMessage = new StringBuilder(messagesService.getReplyText("subscription.trainTicketsPriceChanges",
-                subscription.getLink(), subscription.getPrice())).append(priceChangeMessage);
-
-        notificationMessage.append(messagesService.getReplyText("subscription.lastTicketPrices"));
-
-        telegramBot.sendMessage(subscription.getChatId(), notificationMessage.toString());
+        Item item = webParserService.getItem(subscription.getLink());
+        if (subscription.getPrice() < item.getPrice()) {
+            StringBuilder notificationMessage = new StringBuilder(messagesService.getReplyText("subscription.trainTicketsPriceChanges",
+                    subscription.getLink(), subscription.getPrice(), subscription.getLink()));
+            telegramBot.sendMessage(subscription.getChatId(), notificationMessage.toString());
+        }
     }
 
 }
